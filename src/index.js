98d3760dbb8e646b29ab844ad9f0747b8985fe33
kozr <zxzflow@gmail.com>
2021-06-06 00:28:53 -0700

    formatted file

--- src/index.js
+++ src/index.js
@@ -1,25 +1,30 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './pages/App';
-import Input from './components/Input'
-import Charcuterie from 'pages/Charcuterie';
-import reportWebVitals from './utils/reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./pages/App";
+import Input from "./components/Input";
+import Charcuterie from "pages/Charcuterie";
+import reportWebVitals from "./utils/reportWebVitals";
 
 ReactDOM.render(
   <React.StrictMode>
     {/* <App /> */}
-    <Charcuterie >
+    <Charcuterie>
       <p>Hi</p>
-      <Input size={"small"} placeholder={"Password"} type={"password"}/>
-      <Input size={"large"}/>
+      <Input size={"small"} placeholder={"Password"} type={"password"} />
+      <Input size={"large"} />
       <br />
-      <Input size={"default"} label={"default"} placeholder={"disabled"} disabled={true} />
+      <Input
+        size={"default"}
+        label={"default"}
+        placeholder={"disabled"}
+        disabled
+      />
       <br />
-      <Input size={"medium"} label={"side by side"} placeholder={"username"}/>
+      <Input size={"medium"} label={"side by side"} placeholder={"username"} />
     </Charcuterie>
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
 
 // If you want to start measuring performance in your app, pass a function
