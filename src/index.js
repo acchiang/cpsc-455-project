fba6fca6bd61fb5bb2edb4ffa327c823d5934c9a
rzyxie <rzyxie>
2021-06-09 15:20:54 -0700

    updated charcuterie page

--- src/index.js
+++ src/index.js
@@ -3,27 +3,29 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import Charcuterie from 'pages/Charcuterie';
 import reportWebVitals from './utils/reportWebVitals';
+import TextIcon from 'components/TextIcon'
 import Button from 'components/Button'
 
 ReactDOM.render(
   <React.StrictMode>
     <Charcuterie >
-      <p>Hi</p>
+      <h2>User Icons</h2>
       <TextIcon textLetter={'a'} size={'small'} color={'#8EDB31'}>small</TextIcon>
       <TextIcon textLetter={'b'} size={'default'} color={'#31B4DB'}>default</TextIcon>
-      <h3>small</h3>
+      <h2>Buttons</h2>
+      <p>small</p>
       <Button size={"small"} type={"primary"} label={"primary"} />
       <Button size={"small"} type={"secondary"} label={"secondary"} />
       <Button size={"small"} type={"text"} label={"text"} />
-      <h3>medium</h3>
+      <p>medium</p>
       <Button size={"medium"} type={"primary"} label={"primary"} />
       <Button size={"medium"} type={"secondary"} label={"secondary"} />
       <Button size={"medium"} type={"text"} label={"text"} />
-      <h3>large</h3>
+      <p>large</p>
       <Button size={"large"} type={"primary"} label={"primary"} />
       <Button size={"large"} type={"secondary"} label={"secondary"} />
       <Button size={"large"} type={"text"} label={"text"} />
-      <h3>others</h3>
+      <p>others</p>
       <Button label={"default"} />
       <Button label={"click me!"} onClick={() => { alert("button clicked"); }} />
       <br />
