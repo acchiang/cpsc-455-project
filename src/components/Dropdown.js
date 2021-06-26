b901b87cc30e9118424257e78133e552ba7e6a4f
Allison <acchiang98@gmail.com>
2021-06-25 18:53:52 -0700

    add dropdown container for inline-block

--- src/components/Dropdown.js
+++ src/components/Dropdown.js
@@ -11,6 +11,7 @@ padding: 10px;
 border-radius: 5px;
 margin: 0.25em;
 padding: 0.25em 0.25em;
+display: inline-block;
 `;
 
 const Dropdown = ({ options, defaultOption, size, showInput, setShowInput, customValue, ...props }) => {
@@ -24,13 +25,17 @@ const Dropdown = ({ options, defaultOption, size, showInput, setShowInput, custo
         }
     }
 
+    const DropdownContainer = styled.div`
+        display: inline-block;
+    `
+
     return (
-        <div>
+        <DropdownContainer>
             <DropdownSelect size={size} value={selected} onChange={(e) => handleSelect(e, customValue)}>
                 {options.map((option, key) => { return (<option value={option} key={key}>{option}</option>) })}
             </DropdownSelect>
             {showInput && <Input size={size} placeholder={customValue} />}
-        </div>
+        </DropdownContainer>
     )
 }
 
