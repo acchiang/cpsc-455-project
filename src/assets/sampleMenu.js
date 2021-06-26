27b0d1baae6dfa66722f3807c312e2c0d99c2d45
Allison <acchiang98@gmail.com>
2021-06-25 17:34:46 -0700

    add sample menu

--- src/assets/sampleMenu.js
+++ src/assets/sampleMenu.js
@@ -10,7 +10,44 @@ const menu = [
     {
         name: "mint ice cream",
         price: 5
+    },
+    {
+        name: "tuna don",
+        price: 10
+    },
+    {
+        name: "enchiladas",
+        price: 3
+    },
+    {
+        name: "oreo mcflurry",
+        price: 5
+    },
+    {
+        name: "tuna don",
+        price: 10
+    },
+    {
+        name: "falafel",
+        price: 3
+    },
+    {
+        name: "shoyu ramen",
+        price: 12
+    },
+    {
+        name: "beef tripe pho",
+        price: 9
+    },
+    {
+        name: "butter chicken and naan",
+        price: 13
+    },
+    {
+        name: "tiramisu",
+        price: 5
     }
+    
 ]
 
 export default menu
