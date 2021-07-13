db7b67837a0f943ffb6ddc6ef2b11c7b4f81d030
rzyxie <rzyxie>
2021-07-12 17:41:11 -0700

    set up express (not working)

---
+++ server/routes/menu.js
@@ -0,0 +1,58 @@
+var express = require('express');
+var router = express.Router();
+
+var menu = [
+    {
+        name: "salmon spaghetti",
+        price: 10
+    },
+    {
+        name: "rice pudding",
+        price: 3
+    },
+    {
+        name: "mint ice cream",
+        price: 5
+    },
+    {
+        name: "enchiladas",
+        price: 3
+    },
+    {
+        name: "oreo mcflurry",
+        price: 5
+    },
+    {
+        name: "tuna don",
+        price: 10
+    },
+    {
+        name: "falafel",
+        price: 3
+    },
+    {
+        name: "shoyu ramen",
+        price: 12
+    },
+    {
+        name: "beef tripe pho",
+        price: 9
+    },
+    {
+        name: "butter chicken and naan",
+        price: 13
+    },
+    {
+        name: "tiramisu",
+        price: 5
+    }
+]
+
+/* GET: menu items */
+router.get('/', function (req, res) {
+    res.status(200);
+    console.log(menu);
+    res.send(menu);
+});
+
+module.exports = router;
--- src/pages/OrderScreen.js
+++ src/pages/OrderScreen.js
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from "react";
+import axios from "axios";
 import Theme from "styles/Theme";
 import { OrderContext } from "utils/Context";
-import sampleMenu from "assets/sampleMenu";
 import BackButton from "components/BackButton";
 import Button from "components/Button";
 import MenuSelector from "components/MenuSelector";
@@ -11,6 +11,8 @@ import TotalAmount from "components/TotalAmount";
 import styled from "styled-components";
 import TextIcon from "components/TextIcon";
 import TopTitleBar from "components/TopTitleBar";
+
+const serverURL = "http://localhost:3001/";
 
 const PageContainer = styled.div`
   height: 100vh;
@@ -57,21 +59,24 @@ const optionsNoInput = ["10%", "15%", "20%"];
 function OrderScreen() {
   const [order, setOrder] = useState(null);
   const [subtotal, setSubtotal] = useState(0);
+  const [menu, setMenu] = useState([]);
   const [tipPercent, setTipPercent] = useState(optionsNoInput[0]);
 
   // TODO: get menu from server
   const fetchMenu = () => {
-    return sampleMenu;
+    return axios
+      .get(serverURL)
+      .then((response) => setMenu(response.data));
   };
 
   // TODO: send order to server & attach userId
-  const sendOrder = () => {};
+  const sendOrder = () => { };
 
   // TODO: get group total (including tips) from server
-  const getGroupTotals = () => {};
+  const getGroupTotals = () => { };
 
   // TODO: history.push to next page with data
-  const consolidateOrder = () => {};
+  const consolidateOrder = () => { };
 
   const updateQuantity = (name, quantity) => {
     const updatedOrder = order;
@@ -83,13 +88,13 @@ function OrderScreen() {
     );
   };
 
-  useEffect(() => {
-    // TODO: Perhaps implement webhook (socket) to listen for additional users
-    const initializeOrder = () => {
-      const menu = fetchMenu();
+  useEffect(async () => {
+    // TODO: Perhaps implement webhook (socket) to listen for additional users 
+    const initializeOrder = async () => {
+      await fetchMenu();
       return menu.map((item) => ({ ...item, quantity: 0 }));
     };
-    setOrder(initializeOrder());
+    setOrder(await initializeOrder());
   }, []);
 
   return (
@@ -99,7 +104,7 @@ function OrderScreen() {
           <TopTitleBar
             title={"nw++ Picnic"}
             backUrl={"/create-session"}
-                copyUrl={"https://localhost:3000/order-screen"}
+            copyUrl={"https://localhost:3000/order-screen"}
           />
           <PanelContainer>
             <table>
