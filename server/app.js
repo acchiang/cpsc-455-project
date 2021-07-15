fdba77d81946899afc2392fbb97efbaa477103ae
Christy Lo <christylo@Christys-MacBook-Air.local>
2021-07-14 23:27:44 -0700

    need help with mongoose connection

--- server/app.js
+++ server/app.js
@@ -8,6 +8,7 @@ var cors = require('cors');
 var menuRouter = require('./routes/menu');
 
 var app = express();
+const apiPort = 9000
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -15,7 +16,7 @@ app.set('view engine', 'jade');
 
 app.use(logger('dev'));
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(cors());
@@ -37,5 +38,11 @@ app.use(function(err, req, res, next) {
   res.status(err.status || 500);
   res.render('error');
 });
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.listen(apiPort, () => console.log(`Server running on port ${apiPort}`))
 
 module.exports = app;
